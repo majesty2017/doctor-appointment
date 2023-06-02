52ded889f55f69574ba719ee5a37f677d0a70cd2
urhitech <admin@urhitech.com>
2023-06-02 15:22:39 +0000

    User model created

--- models/User.js
+++ models/User.js
@@ -1,6 +1,21 @@
 const mongoose = require('mongoose');   
 
-const UserSchema = mongoose.Schema({})
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required']
+    },
+    
+    email: {
+        type: String,
+        required: [true, 'Email is required']
+    },
+
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+})
 
 const User = mongoose.model('users', UserSchema)
 
