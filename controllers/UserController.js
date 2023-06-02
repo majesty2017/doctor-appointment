b4f2dc05747b75fa6fa8cb6d14526e72f1d2c291
urhitech <admin@urhitech.com>
2023-06-02 16:48:05 +0000

    Login

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,27 +1,52 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 
+const jwt = require('jsonwebtoken')
+
 const RegisterController = async (req, res) => {
   try {
     const existingUser = await User.findOne({
       email: req.body.email,
     });
     if (existingUser) {
-        return res.status(200).send({success: false, message: 'User already exists!'})
+      return res
+        .status(200)
+        .send({ success: false, message: 'User already exists!' });
     }
-    const password = req.body.password
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
-    req.body.password = hashedPassword
-    const user = new User(req.body)
-    await user.save()
-    res.status(201).send({success: true, message: 'Registered successfully!'})
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    req.body.password = hashedPassword;
+    const user = new User(req.body);
+    await user.save();
+    res
+      .status(201)
+      .send({ success: true, message: 'Registered successfully!' });
   } catch (error) {
     console.log(error);
     res.status(500).send({ success: false, message: error });
   }
 };
 
-const LoginController = async (req, res) => {};
+const LoginController = async (req, res) => {
+  try {
+    const user = await User.findOne({email: req.body.email})
+    if (!user) {
+        return res
+          .status(200)
+          .send({ success: false, message: 'User does\'nt exists!' });
+    }
+    const isMatch = await bcrypt.compare(req.body.password, user.password)
+    if (!isMatch) {
+        return res
+          .status(200)
+          .send({ success: false, message: "Invalid email or password!" });
+    }
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ success: false, message: error });
+  }
+};
 
 module.exports = { LoginController, RegisterController };
